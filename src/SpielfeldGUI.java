8268d80eccc5e1375680538dfa05c9f122342515
mstein1808 <mstein1808@gmail.com>
2013-11-18 22:38:05 +0100

    Marian: erste Implementierung der ImagIcon-Resourcen in SpielfeldGUI, funktionert aber zurzeit noch nicht.

--- src/SpielfeldGUI.java
+++ src/SpielfeldGUI.java
@@ -1,19 +1,39 @@
+import java.net.URL;
 import java.util.HashMap;
 
 import javax.swing.ImageIcon;
-import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 
-public class SpielfeldGUI extends JComponent {
+public class SpielfeldGUI extends JPanel {
 	
 	private Spielfeld spielfeld;
-	private static HashMap<String, ImageIcon> icons;
+	private HashMap<String, ImageIcon> icons;
 	
-	//static-block, um die bildresourcen einmal zu laden
 	
-	static{
-		ImageIcon icon = new ImageIcon();
+	public SpielfeldGUI(){
+		
+		//Bilder laden
+		URL s = SpielfeldGUI.class.getClassLoader().getResource("res/img/schwarz_leer.png");
+		ImageIcon icon = new ImageIcon(getClass().getResource("/res/img/schwarz_leer.png"));
+		icons.put("schwarz_leer", icon);
+		
+		icon = new ImageIcon(getClass().getResource("/res/img/schwarz_voll.png"));
+		icons.put("schwarz_voll", icon);
+		
+		icon = new ImageIcon(getClass().getResource("/res/img/schwarz_dame.png"));
+		icons.put("schwarz_dame", icon);
+		
+		icon = new ImageIcon(getClass().getResource("/res/img/weiss_leer.png"));
+		icons.put("weiss_leer", icon);
+		
+		icon = new ImageIcon(getClass().getResource("/res/img/weiss_voll.png"));
+		icons.put("weiss_voll", icon);
+		
+		icon = new ImageIcon(getClass().getResource("/res/img/weiss_dame.png"));
+		icons.put("weiss_dame", icon);
+		
+		
 	}
-	
 
 }
